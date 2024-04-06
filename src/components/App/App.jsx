ac68c446f24c5dc0be02a26fa3795f35ec52ded8
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-06 23:01:07 +0300

    step-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
 import contactsArray from '../../../contacts.json'
-import ContactForm from '../ContactForm/ContactForm'
+import ContactList from '../ContactList/ContactList'
 
 import css from './App.module.css'
 export default function App() {
     return (
         <div className={css.container}>
             <h1 className={css.mainTitle}>Phonebook</h1>
-  <ContactForm />
-  {/* <SearchBox />
-  <ContactList /> */}
+  
+  
+            <ContactList contactList={ contactsArray} />
 
 
         </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,24 @@
 import css from './Contact.module.css'
+import { BsPhone, BsPerson, BsTrash } from "react-icons/bs";
 
-const Contact = () => {
-    <div className={containerContac}>
-       
-    </div>
+
+export default function Contact({data:{ name, number }} ) {
+    return (<div className={css.containerContact}>
+        <div className={css.thumbContact}>
+            <h2 className={css.nameContact}>
+                <BsPerson size="22" />
+                {name}
+            </h2>
+            <p className={css.numberContact}>
+                <BsPhone size="22" />
+                {number}
+            </p>
+        </div>
+        <button className={css.buttonDelete} onClick={() => { }}>
+            <BsTrash size="15" />
+            Delete</button>
+    </div>);
+    
 
 }
-export default Contact
+
