9e895f9259103efd93b13e1783728f0fa921d43f
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-07 10:42:48 +0300

    step-3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import SearchBox from '../SearchBox/SearchBox';
 import { useState } from 'react';
 
 import css from './App.module.css'
+import ContactForm from '../ContactForm/ContactForm';
 export default function App() {
     const [contacts, setContacts] = useState(contactsArray)
     const [search, setSearch] = useState("");
@@ -12,9 +13,15 @@ export default function App() {
     contact.name.toLowerCase().includes(search.toLowerCase())
   );
 
+    const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
     return (
         <div className={css.container}>
             <h1 className={css.mainTitle}>Phonebook</h1>
+            <ContactForm onAdd={addContact}/>
             <SearchBox value={search} onFilter={setSearch}/>
   
   
