6d1fd11836fd7a8422a70ce335461ddeed33ebe2
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-07 11:24:13 +0300

    step-5

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,23 @@
 import contactsArray from '../../../contacts.json';
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import css from './App.module.css'
 import ContactForm from '../ContactForm/ContactForm';
+
 export default function App() {
-    const [contacts, setContacts] = useState(contactsArray)
-    const [search, setSearch] = useState("");
+   
+    const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("saved-contacts");
+    return savedContacts ? JSON.parse(savedContacts) : contactsArray;
+    });
+    
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
 
     const filterContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(search.toLowerCase())
@@ -26,15 +36,22 @@ export default function App() {
     };
     
     return (
-        <div className={css.container}>
-            <h1 className={css.mainTitle}>Phonebook</h1>
-            <ContactForm onAdd={addContact}/>
-            <SearchBox value={search} onFilter={setSearch}/>
-  
-  
-            <ContactList contactList={ filterContacts} onDelete={deleteContact} />
+        <>
+            <div className={css.container}>
+        <h1 className={css.mainTitle}>Phonebook </h1>
+        <ContactForm onAdd={addContact} />
+        <SearchBox value={search} onFilter={setSearch} />
 
+        {contacts.length !== 0 ? (
+          <ContactList contactList={filterContacts} onDelete={deleteContact} />
+        ) : (
+          <p className={css.infoText}>Your phonebook is empty </p>
+        )}
 
-        </div>
+        {filterContacts.length === 0 && contacts.length !== 0 && (
+          <p className={css.infoText}>No contacts found </p>
+        )}
+      </div>
+        </>
     )
 }
