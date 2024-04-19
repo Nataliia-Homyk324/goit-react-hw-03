58a1a7a33b6e4a33374e13bb4f942fe6532aef5e
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-19 12:17:59 +0300

    rt

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,42 +2,40 @@ import contactsArray from '../../../contacts.json';
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
 import { useState, useEffect } from 'react';
-import css from './App.module.css'
+import css from './App.module.css';
 import ContactForm from '../ContactForm/ContactForm';
 
 export default function App() {
-   
-    const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem("saved-contacts");
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem('saved-contacts');
     return savedContacts ? JSON.parse(savedContacts) : contactsArray;
-    });
-    
-  const [search, setSearch] = useState("");
+  });
+
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
+    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-
-    const filterContacts = contacts.filter((contact) =>
+  const filterContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(search.toLowerCase())
   );
 
-    const addContact = (newContact) => {
-    setContacts((prevContacts) => {
+  const addContact = newContact => {
+    setContacts(prevContacts => {
       return [...prevContacts, newContact];
     });
-    };
-    
-     const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== contactId);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== contactId);
     });
-    };
-    
-    return (
-        <>
-            <div className={css.container}>
+  };
+
+  return (
+    <>
+      <div className={css.container}>
         <h1 className={css.mainTitle}>Phonebook </h1>
         <ContactForm onAdd={addContact} />
         <SearchBox value={search} onFilter={setSearch} />
@@ -52,6 +50,6 @@ export default function App() {
           <p className={css.infoText}>No contacts found </p>
         )}
       </div>
-        </>
-    )
+    </>
+  );
 }
